4996578d6908bd184dd0d5a1e9a1432830666910
JBurton175 <jburton175@gmail.com>
2025-02-24 18:54:33 -0700

    fixed missing category_id1 error

--- Mission8/Models/Mission8DBContext.cs
+++ Mission8/Models/Mission8DBContext.cs
@@ -8,5 +8,17 @@ namespace Mission8.Models
         public DbSet<Task> Task { get; set; }
         public DbSet<Categories> Category { get; set; }
 
+        public Mission8DBContext(DbContextOptions<Mission8DBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .HasOne(t => t.Category)
+                .WithMany()
+                .HasForeignKey(t => t.category_id);
+        }
     }
 }
